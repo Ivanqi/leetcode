4f98daf9cdae3c2b14f3c95ef8de43cc5ace7f4c
Ivanqi <512350225@qq.com>
2020-04-15 20:40:56 +0800

    【开发】leetcode49/c++版本

---
+++ 49/leetcode_49_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        vector<vector<string>> groupAnagrams(vector<string>& strs) {
+            map<string,vector<string> > ma;
+            vector<vector<string>> res;
+
+            for(auto str:strs){
+                string tmp = str;
+                sort(tmp.begin(),tmp.end());
+                ma[tmp].push_back(str);
+            }
+
+            for(const auto& m:ma) {
+                res.push_back(m.second);
+            }
+            return res;
+        
+        }
+};
+
+void test_case_1(Solution So) {
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+    vector<vector<string>> ret = So.groupAnagrams(strs);
+}
+int main() {
+    Solution So;
+    test_case_1(So);
+}
