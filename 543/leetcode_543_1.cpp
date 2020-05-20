7deb81d589984e1693687f4c4cb75fcc536151e9
Ivanqi <512350225@qq.com>
2020-05-20 10:03:24 +0800

    【开发】leetcode543/c++

---
+++ 543/leetcode_543_1.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+    public:
+        int ans;
+        int bfs(TreeNode *root) {
+            if (root == NULL) return 0;
+            int L = bfs(root->left);
+            int R = bfs(root->right);
+            ans = max(ans, L + R + 1);
+            return max(L, R) + 1;
+
+        }
+
+        int diameterOfBinaryTree(TreeNode* root) {
+            ans = 1;
+            bfs(root);
+            return ans - 1;
+        }
+
+        void insert(TreeNode* &root, vector<int>& v) {
+            
+            int maxNum = v.size();
+            queue<int> Q;
+            Q.push(0);
+
+            if (root == NULL) {
+                root = createNode(v[0]);
+            }
+
+            queue <TreeNode *> QN;
+            TreeNode *node = root;
+
+            QN.push(root);
+
+            while (!QN.empty() && !Q.empty()) {
+                node = QN.front();
+                QN.pop();
+
+                int cur = Q.front();
+                Q.pop();
+
+                int leftNum = 2 * cur + 1;
+                int rightNum = 2 * cur + 2;
+                if (leftNum < maxNum && v[leftNum] > 0 && node->left == NULL) {
+                    node->left = createNode(v[leftNum]);
+                    QN.push(node->left);
+                    Q.push(leftNum);
+                }
+                
+                if (rightNum < maxNum && v[rightNum] > 0 && node->right == NULL) {
+                    node->right = createNode(v[rightNum]);
+                    QN.push(node->right);
+                    Q.push(rightNum);
+                }
+            }
+        }
+
+        TreeNode* createNode(int val)
+        {
+            TreeNode* tmp = (TreeNode*)malloc(sizeof(TreeNode));
+            tmp->val = val;
+            tmp->left = NULL;
+            tmp->right = NULL;
+        }
+};
+
+void test_case_1(Solution So) {
+    vector<int> x = {1, 2, 3, 4, 5};
+    TreeNode *root = NULL;
+
+    So.insert(root, x);
+
+    int ret = So.diameterOfBinaryTree(root);
+    cout << "结果为: " << ret << endl;
+}
+
+int main() {
+
+    Solution So;
+    test_case_1(So);
+}
---
+++ 543/readme.md
@@ -0,0 +1,16 @@
+#### 543. 二叉树的直径
+- 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+- 示例 :
+```
+         1
+         / \
+        2   3
+       / \     
+      4   5    
+```
+  - 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+- 注意：两结点之间的路径长度是以它们之间边的数目表示。
+
+#### 参考资料
+- [二叉树的直径](https://leetcode-cn.com/problems/diameter-of-binary-tree/solution/er-cha-shu-de-zhi-jing-by-leetcode-solution/)
+- [详解官方递归代码的运行过程（Pthon3、C++）](https://leetcode-cn.com/problems/diameter-of-binary-tree/solution/shi-pin-jie-shi-di-gui-dai-ma-de-yun-xing-guo-chen/)
