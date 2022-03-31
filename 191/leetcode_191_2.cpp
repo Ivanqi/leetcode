e4ce715dc346c8f6b35b964ffe93fd0ab2bea56c
Ivanqi <512350225@qq.com>
2022-03-31 21:58:53 +0800

    【开发】leetcode191

--- 191/leetcode_191_2.cpp
+++ 191/leetcode_191_2.cpp
@@ -5,12 +5,10 @@ class Solution {
     public:
         int hammingWeight(uint32_t n) {
             int ret = 0;
-            for (int i = 0; i < 32; i++) {
-                if (n & (1 << i)) {
-                    ret++;
-                }
+            while (n) {
+                n &= n - 1;
+                ret++;
             }
-
             return ret;
         }
 };
---
+++ 191/readme.md
@@ -0,0 +1,2 @@
+# 参考资料
+- [【负雪明烛】详解位运算，附本题躲坑指南](https://leetcode-cn.com/problems/number-of-1-bits/solution/fu-xue-ming-zhu-xiang-jie-wei-yun-suan-f-ci7i/)
