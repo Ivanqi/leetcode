c5a528afc2d97183d41c240bb0c78e26f77a6dee
Ivanqi <512350225@qq.com>
2022-04-09 21:51:43 +0800

    【开发】leetcode217

---
+++ 217/leetcode_217_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+        bool containsDuplicate(vector<int>& nums) {
+            unordered_map<int, int> map;
+
+            for (int i = 0; i < nums.size(); i++) {
+                if (map.find(nums[i]) != map.end()) {
+                    return true;
+                }
+                map[nums[i]] = nums[i];
+
+            }
+            return false;
+        }
+};
