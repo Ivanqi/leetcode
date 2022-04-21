6ad671fb412d1df19e061c3dbc0f7257439cd429
Ivanqi <512350225@qq.com>
2022-04-21 20:37:15 +0800

    【开发】leetcode283

---
+++ 283/leetcode_283_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+        void moveZeroes(vector<int>& nums) {
+            int len = nums.size(), left = 0, right = 0;
+            while (right < len) {
+                if (nums[right]) {
+                    swap(nums[left], nums[right]);
+                    left++;
+                }
+                right++;
+            }
+        }
+};
